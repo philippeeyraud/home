f6753f72523ca4014d0245f5a7087048af391edf
philippeeyraud <philippe.eyraud@ymail.com>
2022-09-28 10:37:53 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,56 @@
-<script setup lang="ts">
-import TheHeader from './components/Header.vue'
-import TheFooter from './components/Footer.vue'
-import Shop from './components/Shop/Shop.vue'
-import Cart from './components/Cart/Cart.vue'
-import data from './data/product';
-import { computed, reactive } from 'vue';
-import type { ProductCartInterface, ProductInterface } from './interfaces';
-import product from './data/product';
-//Je cree une constante et je la type pour dire quel type d'informations je vais stocker.
-//Je fais un import de data pour récupérer mon tableau de data
-//Je transferrt les informations sur les composants qui en ont besoin, ici le composant Shop va lister toute la liste de mes produits
-//Je passe une liste de produit à mon composant shop
-//const products = reactive<ProductInterface[]>(data)
-
-//Pouvoir accepter des produits et pouvooir les déclarer dans notre panier
-//On initialise avec un tableau vide
-//const cart = reactive<ProductInterface[]>([]);
-//Créer une proprieté réactive(state) pour pouvoir décrir tous les éléments dont on aura besoin
-const state = reactive<{
-products:ProductInterface[] ,
-cart:ProductCartInterface[]
-}>({
-  products: data,
-  cart: []
-})
-//Crééer la fonction qui va nous permettre d ajouter un produit dans notre panier
-//On fait en sorte que lorsque cette fonction est invoqué elle recoive l id
-//On implemente cette fonction
-//J ajoute mon produit issue de la liste des produits
-//Je dissocie  le produit qui est dans mon state , je vais créer un nv produit mais il ne faut pas qu il ait la ^m reference
-//On fait la déconstruction avec les 3.
-//On va écouter l evenement depuis notre shop
-function addProductToCart(productId: number): void{
-const product = state.products.find(product => product.id === productId);
-//if(product && !state.cart.find(product => product.id === productId)) {
- // state.cart.push({...product, quantity: 1 })
-//}
-if(product) {
-  const productInCart = state.cart.find(product => product.id === productId)
-  if (productInCart){
-    productInCart.quantity++
-  } else {
-    state.cart.push({...product, quantity: 1 })
-  }
-}
-}
-//On déclare la fonction quui va nous permettre de supprimer l element de notre panier
-//On supprime l'element de notre cart,on va s assure que tous les elements qui sont dans le panier n'ont pas l'id de celui que je supprime
-
-function removeProductFromCart(productId: number): void {
-
-const productFromCart = state.cart.find(product => product.id === productId);
-if(productFromCart) {
-  if (productFromCart.quantity === 1) {
-    state.cart = state.cart.filter(product => product.id !== productId);
-  } else {
-    productFromCart.quantity--;
-  }
-}
-}
-const cartEmpty = computed(() => state.cart.length === 0)
-</script>
-
 <template>
-  <div class="app-container" :class="{
-    gridEmpty: cartEmpty
-  }">
-    <TheHeader class="header" />
-    <Shop :products="state.products" @add-product-to-cart="addProductToCart" class="shop" />
-  <!--Si il ny a rien ds le panier je ne l'affiche pas je vais utiliser un v-if pour voir si il y a qql chose dans cart-->
-    <Cart 
-    v-if="!cartEmpty "
-      :cart="state.cart" class="cart " @remove-product-from-cart="removeProductFromCart"/>
-    <TheFooter class="footer " />
-  </div>
+  <input
+    v-on="validationListeners"
+    v-model="usernameValue"
+    type="text"
+  />
+  <p v-if="usernameError">{{ usernameError }}</p>
+
+  <pre>
+    {{ usernameValue }}
+  </pre>
 </template>
 
-<style lang="scss">
-@import './assets/scss/base.scss';
-@import './assets/scss/debug.scss';
+<script setup lang="ts">
+import { useForm, useField } from "vee-validate";
+import { z } from "zod";
+import { computed } from "vue";
+import { toFormValidator } from "@vee-validate/zod";
 
-.app-container {
-  min-height: 100vh;
-  display: grid;
-  grid-template-areas: 'header header' 'shop cart' 'footer footer';
-  grid-template-columns: 75% 25%;
-  grid-template-rows: 48px auto 48px;
-}
-.gridEmpty {
-  grid-template-areas: 'header ' 'shop' 'footer ';
-  grid-template-columns: 100%;
+const validationSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Le champ est trop court" })
+    .max(10, { message: "Le champ est trop long" }),
+});
 
-}
+const validationListeners = computed(() => {
+  //  Validation douce si pas encore d'erreur :
+  if (!usernameError.value) {
+    return {
+      blur: handleChange,
+      change: handleChange,
+    };
+  } else {
+    //  Validation agressive en cas d'erreur :
+    return {
+      blur: handleChange,
+      change: handleChange,
+      input: handleChange,
+    };
+  }
+});
 
+useForm({
+  validationSchema: toFormValidator(validationSchema),
+  initialValues: { username: "Test" },
+});
 
+const {
+  value: usernameValue,
+  errorMessage: usernameError,
+  handleChange,
+} = useField("username", null, { validateOnValueUpdate: false });
+</script>
 
-.header {
-  grid-area: header;
-}
-.shop {
-  grid-area: shop;
-}
-.cart {
-  grid-area: cart;
-  border-left: var(--border);
-  background-color: white;
-}
-.footer {
-  grid-area: footer;
-}
-</style>
+<style scoped lang="scss"></style>
